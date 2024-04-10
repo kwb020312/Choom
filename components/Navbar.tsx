1fc3b2219f2b68f65ebd1bd553e498dffe32a7aa
김우빈 <kwb020312@naver.com>
2024-04-10 18:37:50 +0900

    Fix (Sidebar): Select Effect

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,5 +1,5 @@
 const Navbar = () => {
-  return <div></div>;
+  return <nav className="fixed z-50 w-full bg-dark-1 px-6 py-4 lg:px-10"></nav>;
 };
 
 export default Navbar;
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,15 +4,15 @@ import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { useState } from "react";
 
 const Sidebar = () => {
-  const pathName = usePathname();
+  const [selectedTab, setSelectedTab] = useState("/");
   return (
     <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
       <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map(({ imgUrl, label, route }) => {
-          const isActive = pathName === route || pathName.startsWith(route);
+          const isActive = selectedTab === route;
 
           return (
             <Link
@@ -22,6 +22,7 @@ const Sidebar = () => {
                 "flex gap-4 items-center p-4 rounded-lg justify-start",
                 isActive && "bg-blue-1"
               )}
+              onClick={() => setSelectedTab(route)}
             >
               <Image src={imgUrl} alt={label} width={24} height={24} />
               <p className="text-lg font-semibold max-lg:hidden">{label}</p>
