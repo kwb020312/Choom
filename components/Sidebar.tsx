9f9df2101ff1fa6d52bc8fe3a7ab0e280c101f35
김우빈 <kwb020312@naver.com>
2024-04-10 19:02:25 +0900

    Feat (Sidebar): Sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -4,16 +4,16 @@ import { sidebarLinks } from "@/constants";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { usePathname } from "next/navigation";
 
 const Sidebar = () => {
-  const [selectedTab, setSelectedTab] = useState("/");
+  const pathName = usePathname();
   return (
     <section className="sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]">
       <div className="flex flex-1 flex-col gap-6">
         {sidebarLinks.map(({ imgUrl, label, route }) => {
-          const isActive = selectedTab === route;
-
+          const isActive =
+            pathName === route || pathName.startsWith(`/${route}/`);
           return (
             <Link
               href={route}
@@ -22,7 +22,6 @@ const Sidebar = () => {
                 "flex gap-4 items-center p-4 rounded-lg justify-start",
                 isActive && "bg-blue-1"
               )}
-              onClick={() => setSelectedTab(route)}
             >
               <Image src={imgUrl} alt={label} width={24} height={24} />
               <p className="text-lg font-semibold max-lg:hidden">{label}</p>
