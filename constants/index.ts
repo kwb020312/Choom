37c6bb03e444d91b759dbef703a68c7d43f51dce
김우빈 <kwb020312@naver.com>
2024-04-14 22:19:19 +0900

    Feat (Page): Previous Meeting page

--- constants/index.ts
+++ constants/index.ts
@@ -30,6 +30,6 @@ export const avatarImages = [
   "/images/avatar-1.jpeg",
   "/images/avatar-2.jpeg",
   "/images/avatar-3.jpeg",
-  "/images/avatar-4.jpeg",
-  "/images/avatar-5.jpeg",
+  "/images/avatar-4.png",
+  "/images/avatar-5.png",
 ];
