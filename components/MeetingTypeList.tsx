379545e06ff4d566ae5a0d36678d19a04267d143
김우빈 <kwb020312@naver.com>
2024-04-14 18:21:22 +0900

    Feat (Share): Meeting Link Share

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -8,6 +8,7 @@ import { useUser } from "@clerk/nextjs";
 import { Call, useStreamVideoClient } from "@stream-io/video-react-sdk";
 import { useToast } from "./ui/use-toast";
 import { Textarea } from "./ui/textarea";
+import ReactDatePicker from "react-datepicker";
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -65,6 +66,9 @@ const MeetingTypeList = () => {
       });
     }
   };
+
+  const meetingLink = `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${callDetails?.id}`;
+
   return (
     <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
       <HomeCard
@@ -107,7 +111,27 @@ const MeetingTypeList = () => {
             <label className="text-base leading-[22px] text-sky-2">
               Add a description
             </label>
-            <Textarea className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0" />
+            <Textarea
+              className="border-none bg-dark-3 focus-visible:ring-0 focus-visible:ring-offset-0"
+              onChange={(e) =>
+                setValues({ ...values, description: e.target.value })
+              }
+            />
+          </div>
+          <div className="flex w-full flex-col gap-2.5">
+            <label className="text-base leading-[22px] text-sky-2">
+              Select Date and Time
+            </label>
+            <ReactDatePicker
+              selected={values.dateTime}
+              onChange={(date) => setValues({ ...values, dateTime: date! })}
+              showTimeSelect
+              timeFormat="HH:mm"
+              timeIntervals={15}
+              timeCaption="time"
+              dateFormat={"MMMM d, yyyy h:mm aa"}
+              className="w-full rounded bg-dark-3 p-2 focus:outline-none"
+            />
           </div>
         </MeetingModal>
       ) : (
@@ -118,7 +142,7 @@ const MeetingTypeList = () => {
           className="text-center"
           buttonText="Copy Meeting Link"
           handleClick={() => {
-            // navigator.clipboard.writeText(meetingLink);
+            navigator.clipboard.writeText(meetingLink);
             toast({ title: "Copied to clipboard" });
           }}
           image="/icons/checked.svg"
