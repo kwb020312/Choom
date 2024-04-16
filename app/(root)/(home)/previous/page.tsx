1bc259736e9598b4444de45f8b7a589a01b453d6
김우빈 <kwb020312@naver.com>
2024-04-16 22:20:18 +0900

    Feat (Previous): 한국어 번역 추가

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -3,7 +3,7 @@ import CallList from "@/components/CallList";
 const Previous = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">Previous</h1>
+      <h1 className="text-3xl font-bold">이전 회의</h1>
 
       <CallList type="ended" />
     </section>
