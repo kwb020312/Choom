52715cf0038a767949371165cfaa191506f96a6c
김우빈 <kwb020312@naver.com>
2024-04-16 22:19:45 +0900

    Feat (Upcomming): 한국어 번역 추가

--- app/(root)/(home)/upcoming/page.tsx
+++ app/(root)/(home)/upcoming/page.tsx
@@ -3,7 +3,7 @@ import CallList from "@/components/CallList";
 const Upcoming = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">Upcoming</h1>
+      <h1 className="text-3xl font-bold">다가올 회의</h1>
 
       <CallList type="upcoming" />
     </section>
--- components/CallList.tsx
+++ components/CallList.tsx
@@ -1,29 +1,25 @@
-// @ts-nocheck
-
 "use client";
 
-import { useGetCalls } from "@/hooks/useGetCalls";
 import { Call, CallRecording } from "@stream-io/video-react-sdk";
-import { useRouter } from "next/navigation";
-import { useState, useEffect } from "react";
-import MeetingCard from "./MeetingCard";
+
 import Loader from "./Loader";
-import { useToast } from "./ui/use-toast";
+import { useGetCalls } from "@/hooks/useGetCalls";
+import MeetingCard from "./MeetingCard";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
+  const router = useRouter();
   const { endedCalls, upcomingCalls, callRecordings, isLoading } =
     useGetCalls();
-  const router = useRouter();
   const [recordings, setRecordings] = useState<CallRecording[]>([]);
-
-  const { toast } = useToast();
 
   const getCalls = () => {
     switch (type) {
       case "ended":
         return endedCalls;
       case "recordings":
-        return callRecordings;
+        return recordings;
       case "upcoming":
         return upcomingCalls;
       default:
@@ -34,11 +30,11 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
   const getNoCallsMessage = () => {
     switch (type) {
       case "ended":
-        return "No Previous Calls";
-      case "recordings":
-        return "No Recordings";
+        return "이전 회의 기록이 존재하지 않습니다.";
       case "upcoming":
-        return "No Upcoming Calls";
+        return "회의 일정이 존재하지 않습니다.";
+      case "recordings":
+        return "기록된 녹화가 존재하지 않습니다.";
       default:
         return "";
     }
@@ -46,35 +42,33 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
 
   useEffect(() => {
     const fetchRecordings = async () => {
-      try {
-        const callData = await Promise.all(
-          callRecordings.map((meeting) => meeting.queryRecordings())
-        );
+      const callData = await Promise.all(
+        callRecordings?.map((meeting) => meeting.queryRecordings()) ?? []
+      );
 
-        const recordings = callData
-          .filter((call) => call.recordings.length > 0)
-          .flatMap((call) => call.recordings);
+      const recordings = callData
+        .filter((call) => call.recordings.length > 0)
+        .flatMap((call) => call.recordings);
 
-        setRecordings(recordings);
-      } catch (error) {
-        toast({ title: "Try again later" });
-      }
+      setRecordings(recordings);
     };
 
-    if (type === "recordings") fetchRecordings();
+    if (type === "recordings") {
+      fetchRecordings();
+    }
   }, [type, callRecordings]);
+
+  if (isLoading) return <Loader />;
 
   const calls = getCalls();
   const noCallsMessage = getNoCallsMessage();
-
-  if (isLoading) return <Loader />;
 
   return (
     <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
       {calls && calls.length > 0 ? (
         calls.map((meeting: Call | CallRecording) => (
           <MeetingCard
-            key={(meeting as Call)?.id}
+            key={(meeting as Call).id}
             icon={
               type === "ended"
                 ? "/icons/previous.svg"
@@ -83,33 +77,33 @@ const CallList = ({ type }: { type: "ended" | "upcoming" | "recordings" }) => {
                 : "/icons/recordings.svg"
             }
             title={
-              (meeting as Call).state?.custom?.description?.substring(0, 26) ||
-              meeting?.filename?.substring(0, 20) ||
-              "No Meeting"
+              (meeting as Call).state?.custom?.description ||
+              (meeting as CallRecording).filename?.substring(0, 20) ||
+              "존재하지 않습니다."
             }
             date={
               (meeting as Call).state?.startsAt?.toLocaleString() ||
-              (meeting as Call).start_time.toLocaleString()
+              (meeting as CallRecording).start_time?.toLocaleString()
             }
             isPreviousMeeting={type === "ended"}
-            buttonIcon1={type === "recordings" ? "/icons/play.svg" : undefined}
-            buttonText={type === "recordings" ? "Play" : "Start"}
-            handleClick={() =>
-              router.push(
-                type === "recordings"
-                  ? meeting.url
-                  : `/meeting/${(meeting as Call).id}`
-              )
-            }
             link={
               type === "recordings"
-                ? meeting.url
-                : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${meeting.id}`
+                ? (meeting as CallRecording).url
+                : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${
+                    (meeting as Call).id
+                  }`
+            }
+            buttonIcon1={type === "recordings" ? "/icons/play.svg" : undefined}
+            buttonText={type === "recordings" ? "Play" : "Start"}
+            handleClick={
+              type === "recordings"
+                ? () => router.push(`${(meeting as CallRecording).url}`)
+                : () => router.push(`/meeting/${(meeting as Call).id}`)
             }
           />
         ))
       ) : (
-        <h1>{noCallsMessage}</h1>
+        <h1 className="text-2xl font-bold text-white">{noCallsMessage}</h1>
       )}
     </div>
   );
