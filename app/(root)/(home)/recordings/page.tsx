e628461345197c0843794428b518cc43cd0a836b
김우빈 <kwb020312@naver.com>
2024-04-16 22:20:43 +0900

    Feat (Recording): 한국어 번역 추가

--- app/(root)/(home)/recordings/page.tsx
+++ app/(root)/(home)/recordings/page.tsx
@@ -3,7 +3,7 @@ import CallList from "@/components/CallList";
 const Recordings = () => {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">Recordings</h1>
+      <h1 className="text-3xl font-bold">녹화된 영상</h1>
 
       <CallList type="recordings" />
     </section>
